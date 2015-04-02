3ac744fa3fb822f49f2203b10243897838949dc0
ma huan <418394282@qq.com>
2015-04-02 16:42:25 +0800

    编辑冲突

--- RoutineTest/OperationData/Data/BaseData.cs
+++ RoutineTest/OperationData/Data/BaseData.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +20,8 @@ namespace OperationData
         /// </summary>
         /// <param name="html"></param>
         /// <returns></returns>
-        public abstract List<IInfoModel> ProcessHtml(string html,string pagetag);
-      
+        public abstract List<IInfoModel> ProcessHtml(string html, string pagetag);
+
         public void ExecuteAddEvent(string msg)
         {
             var tmpevent = AddEvent;
@@ -30,35 +30,3 @@ namespace OperationData
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using OperationData.Model;
-
-namespace OperationData
-{
-    public abstract class BaseData
-    {
-        public event Action<string> AddEvent;
-        /// <summary>
-        /// 数据入口
-        /// </summary>
-        public abstract void GetWebData();
-
-        /// <summary>
-        /// 处理html 转换为实体
-        /// </summary>
-        /// <param name="html"></param>
-        /// <returns></returns>
-        public abstract List<IInfoModel> ProcessHtml(string html,string pagetag);
-      
-        public void ExecuteAddEvent(string msg)
-        {
-            if (AddEvent != null)
-                AddEvent(msg);
-        }
-    }
-}
->>>>>>> 8345cbacd5032a91cb4545f981d0905012804cab
